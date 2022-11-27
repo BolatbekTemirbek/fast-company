3df25d1d634039dc08aff48cc59cdd369a014733
unknown <you@example.com>
2022-11-27 18:03:02 +0600

    login

--- src/app/layout/login.jsx
+++ src/app/layout/login.jsx
@@ -1,31 +1,20 @@
-// import React from "react";
-
-// const Login = () => {
-//   return <h1>Login</h1>;
-// };
-
-// export default Login;
 import React from "react";
 import { Formik } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, "Too Short!")
+        .max(70, "Too Long!")
+        .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required")
+});
 
 const Login = () => (
     <div>
-        <h1>Anywhere in your app!</h1>
         <Formik
             initialValues={{ email: "", password: "" }}
-            validate={(values) => {
-                const errors = {};
-                if (!values.email) {
-                    errors.email = "Required";
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
-                        values.email
-                    )
-                ) {
-                    errors.email = "Invalid email address";
-                }
-                return errors;
-            }}
+            validationSchema={SignupSchema}
             onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
